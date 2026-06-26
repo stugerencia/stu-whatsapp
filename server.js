d3fbf0847207797d33480508a2304cda41e3f1c8
stugerencia <stugerencia@gmail.com>
2026-06-26 11:34:01 -0300

    Add Authorization header to CORS configuration

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "80mb" }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
 });
