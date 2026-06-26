ad026187ac2a4b8704ecfce7517d2b186af9c9a3
stugerencia <stugerencia@gmail.com>
2026-06-26 11:40:09 -0300

    Enhance CORS configuration in server.js
    Updated CORS middleware to allow additional HTTP methods and headers.

--- server.js
+++ server.js
@@ -16,9 +16,21 @@ app.use(express.json({ limit: "80mb" }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+
+  const requestedHeaders = req.headers["access-control-request-headers"];
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    requestedHeaders || "Content-Type, Authorization, authorization"
+  );
+
+  res.header("Access-Control-Max-Age", "86400");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
